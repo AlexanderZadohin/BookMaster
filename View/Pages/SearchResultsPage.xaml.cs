56f4964f97af795c032429a57bc7e73254d67320
Sanya Zadohin <seirketa@gmail.com>
2023-03-01 10:50:56 +0300

    Добавьте файлы проекта.

---
+++ View/Pages/SearchResultsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookMaster.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SearchResultsPage.xaml
+    /// </summary>
+    public partial class SearchResultsPage : Page
+    {
+        public SearchResultsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchResultslv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            SearchResultslv.ItemsSource = App.contexnt.BookAuthor.ToList();
+        }
+    }
+}
---
+++ View/Windows/BrowseCatalogWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BookMaster.View.Pages;
+
+namespace BookMaster.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для BrowseCatalogWindow.xaml
+    /// </summary>
+    public partial class BrowseCatalogWindow : Window
+    {
+        public BrowseCatalogWindow()
+        {
+            InitializeComponent();
+
+            SearchResultsFrm.Navigate(new SearchResultsPage());
+        }
+        #region Menu
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void SearchResultsFrm_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
